d8e50661ec58d9e274c711d1ab0cdc4883363b30
Folareen <sakawahab03@gmail.com>
2025-12-10 10:50:54 +0100

    fix?

--- prisma.config.mjs
+++ prisma.config.mjs
@@ -6,6 +6,6 @@ export default defineConfig({
         path: 'prisma/migrations',
     },
     datasource: {
-        url: process.env.DATABASE_URL as string,
+        url: process.env.DATABASE_URL,
     }
 })
